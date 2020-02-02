3c061122bd45febea52789acef9f1ae06f930683
Dahye Jeong <wowowowow@kakao.com>
2020-02-02 21:21:24 +0900

    Updates

---
+++ precache-manifest.75264f260219555b413b548e3de6902a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a381da596a228ae8625d4c48ef091c7",
+    "url": "/realFirstProject/index.html"
+  },
+  {
+    "revision": "956cfd8e2beae2224acc",
+    "url": "/realFirstProject/static/css/main.047a0e7a.chunk.css"
+  },
+  {
+    "revision": "38268b24ffb67aeb4477",
+    "url": "/realFirstProject/static/js/2.64711497.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/realFirstProject/static/js/2.64711497.chunk.js.LICENSE"
+  },
+  {
+    "revision": "956cfd8e2beae2224acc",
+    "url": "/realFirstProject/static/js/main.4e8f6f96.chunk.js"
+  },
+  {
+    "revision": "7dda6d8bbe7ad1dab3f4",
+    "url": "/realFirstProject/static/js/runtime-main.a8434771.js"
+  }
+]);
